2aad12d1c413d32450cad740ed0180de17c37c2d
Diegogt12 <104788402+Diegogt12@users.noreply.github.com>
2022-06-19 13:45:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) throws ExArgumentoIncorrecto {
+    public static void main(String[] args) t {
 	try {
         ParteDeTrabajo p1 = new ParteDeTrabajo("manual", 5, 6.0);
         ParteDeIncidencia p2 = new ParteDeIncidencia("manual", 5, 6.0, 10);
